e4f7c9c3e4c4076551313fa617609b9eb81ec1cc
Christian Lessa <christianclessa@gmail.com>
2021-09-10 12:33:54 -0300

    todo exercicio/5.2 concluido

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -60,4 +60,40 @@ for (let element of numeros) {
 for (let index = 1; index <= 3; index += 1) {
   const tagH3 = document.createElement('h3');
   tagMain.appendChild(tagH3);
+  tagH3.classList.add('description');
 }
+
+//exercicio bônus
+
+// 1. Adicione a classe title na tag h1 criada;
+
+tagH1.classList.add('title');
+
+// 2. Adicione a classe description nas 3 tags h3 criadas;
+
+//coloquei no for do exercicio 9.
+
+// 3. Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+
+tagMain.removeChild(tagSectionLeft);
+     
+
+// 4. Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+
+
+tagSectionRight.style.marginRight = 'auto';
+
+
+// 5. Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+
+
+tagMain.style.backgroundColor = 'green';
+
+
+// 6. Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+
+
+listNotOrder.removeChild(listNotOrder.childNodes[9]);
+listNotOrder.removeChild(listNotOrder.childNodes[8]);
+
+
